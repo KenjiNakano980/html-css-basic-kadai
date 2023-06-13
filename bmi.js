b89e0945ce20d6fed5dd8c6eee634106d57ecfca
KenjiNakano980 <129776124+KenjiNakano980@users.noreply.github.com>
2023-06-14 00:49:13 +0900

    Update bmi.js

--- bmi.js
+++ bmi.js
@@ -1,4 +1,15 @@
-体重：68kg
-身長：1.7m
-計算式：[体重(kg)]÷[身長(m)×身長(m)]
+function keisan(){ 
+  var a, b,d;
+  a = document.getElementById("shincho").value; 
+  b = document.getElementById("taijuu").value;
+  
+  c = b/( (a/100)*(a/100) ); 
 
+ if(c >= 25){ 
+   document.getElementById("kuuran").textContent = "肥満型"; }
+  else if(c < 25 && c >= 18){ 
+   document.getElementById("kuuran").textContent = "標準型"; }
+  else{ 
+    
+document.getElementById("kuuran").textContent = "瘦せ型";}
+ }
